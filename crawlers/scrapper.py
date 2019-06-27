f45ba9321a2fccb186a1621257ced88411ab5060
Adalberto Navarrete <adalberto@hidromares.com.br>
2019-06-27 19:24:26 -0300

    Finalizando scrapper

--- crawlers/scrapper.py
+++ crawlers/scrapper.py
@@ -1,15 +1,15 @@
 import re
+import time
 from urllib.parse import urljoin
 
-import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-from io import StringIO
-import time
 
 REDDIT_OLD_URL = 'https://old.reddit.com/r/'
 HEADERS = {'User-Agent': 'Mozilla/5.0'}
 NUM_REGEX = re.compile('[^0-9eE.]')
+
+FIELDS = ['Pontuação', 'Subreddit', 'Título', 'Comentários', 'Link']
 
 
 class RedditScrapper:
@@ -19,11 +19,10 @@ class RedditScrapper:
         self.min_votes = min_votes
         self.num_pages = num_pages
 
-    def run(self):
-        df = pd.DataFrame(
-            columns=['Pontuação', 'Subreddit', 'Título', 'Comentários',
-                     'Link'])
+    def run(self, format='string'):
+        result = []
         for subreddit in self.subreddits:
+            # initial url for subreddit
             url = urljoin(REDDIT_OLD_URL, subreddit)
             for i in range(self.num_pages):
                 content = requests.get(url, headers=HEADERS).content
@@ -32,32 +31,56 @@ class RedditScrapper:
                     votes = self._parse_votes(thread)
                     if votes >= self.min_votes:
                         link, title, comments = self._parse_links_and_title(
-                            thread)
-                        df = df.append(
-                            [votes, subreddit, link, title, comments])
+                            thread, url)
+                        result.append(
+                            {
+                                key: value for key, value in
+                                zip(FIELDS,
+                                    (votes, subreddit, title, comments, link))
+                            }
+                        )
+                # find next page link and update url for next iteration
                 url = soup.find('span', class_='next-button').find('a').attrs[
                     'href']
+                # slowing dow the scrapper
                 time.sleep(2)
-        output = StringIO()
-        df.to_csv(output, sep='\t', index=False)
-        output.seek(0)
-        return output.read()
+
+        return self._format_result(result, format)
 
     def _parse_votes(self, thread):
         text = thread.find('div', class_='score unvoted').text
+        # deal with new threads w/out votes
         text = text.replace('•', '0')
+        # deal with thousand multipliers (is million necessary?)
         multiplier = {'k': 1e3, 'm': 1e6}
         if text[-1] in multiplier:
             return int(text[:1]) * multiplier[text[-1]]
         return int(text)
 
-    def _parse_links_and_title(self, thread):
+    def _parse_links_and_title(self, thread, url):
         title_link = thread.find('a', class_='title')
+        # outbound links are absoltute but inbound links are relative
+        if 'outbound' in title_link.attrs['class']:
+            title_href = title_link.attrs['href']
+        else:
+            title_href = urljoin(url, title_link.attrs['href'])
         comment_link = thread.find('a', class_='comments')
-        return title_link.attrs['href'], title_link.text, comment_link.attrs[
+        return title_href, title_link.text, comment_link.attrs[
             'href']
+
+    def _format_result(self, result, format):
+        if not result:
+            return "Nenhuma thread bombamdo no momento :("
+        str_list = []
+        for thread in result:
+            str_list.append(
+                '\n'.join(
+                    [f'{key} = {value}' for key, value in thread.items()]))
+        if format == 'list':
+            return str_list
+        return '\n\n'.join(str_list)
 
 
 if __name__ == '__main__':
-    s = RedditScrapper('cats')
+    s = RedditScrapper('cats;programming;brazil', 1000)
     print(s.run())
--- crawlers/telegram_bot.py
+++ crawlers/telegram_bot.py
@@ -1,13 +1,21 @@
-from telegram.ext import Updater, CommandHandler
+import logging
 import os
+
+from scrapper import RedditScrapper
+from telegram.ext import CommandHandler, Updater
+
+logging.basicConfig(filename='bot.log', level=logging.ERROR)
 
 
 def nada_pra_fazer(bot, context, args=[]):
-    pass
+    scrapper = RedditScrapper(*args)
+    for thread in scrapper.run(format='list'):
+        context.message.reply_text(thread)
 
 
 updater = Updater(os.environ.get('TELEGRAM_API_TOKEN', None))
 
-updater.dispatcher.add_handler(CommandHandler('NadaPraFazer', nada_pra_fazer, pass_args=True))
+updater.dispatcher.add_handler(
+    CommandHandler('NadaPraFazer', nada_pra_fazer, pass_args=True))
 updater.start_polling()
 updater.idle()
